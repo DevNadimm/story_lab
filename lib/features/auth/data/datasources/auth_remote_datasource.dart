c8fc8c83dc096f4e1e5272e0cdf5109b27357114
DevNadimm <nadimchowdhury87@gmail.com>
2025-07-01 16:42:03 +0600

    update

---
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -0,0 +1,55 @@
+import 'package:story_lab/core/error/exceptions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDatasource {
+  Future<String> signUpWithEmailAndPassword({
+    required String fullName,
+    required String email,
+    required String password,
+  });
+
+  Future<String> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDatasourceImpl implements AuthRemoteDatasource {
+  final SupabaseClient supabaseClient;
+
+  AuthRemoteDatasourceImpl(this.supabaseClient);
+
+  @override
+  Future<String> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailAndPassword({
+    required String fullName,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        email: email,
+        password: password,
+        data: {
+          'fullName': fullName,
+        },
+      );
+
+      if (response.user == null) {
+        throw ServerException('User is null!');
+      }
+
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
---
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:story_lab/core/error/exceptions.dart';
+import 'package:story_lab/core/error/failures.dart';
+import 'package:story_lab/features/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:story_lab/features/auth/domain/repository/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDatasource authRemoteDatasource;
+
+  AuthRepositoryImpl(this.authRemoteDatasource);
+
+  @override
+  Future<Either<Failure, String>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailAndPassword({
+    required String fullName,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final uid = await authRemoteDatasource.signUpWithEmailAndPassword(
+        fullName: fullName,
+        email: email,
+        password: password,
+      );
+      
+      return right(uid);
+    } on ServerException catch (e) {
+      return left(Failure(message: e.message));
+    }
+  }
+}
