b06e2aa875a4e4c50b7c77cefd03b48c89403e74
DevNadimm <nadimchowdhury87@gmail.com>
2025-07-01 12:23:29 +0600

    update password validators

--- lib/core/utils/validators.dart
+++ lib/core/utils/validators.dart
@@ -22,14 +22,28 @@ class Validators {
     return null;
   }
 
-  /// Validates password (example: minimum 6 characters).
+  /// Validates a password to ensure it meets security requirements:
   static String? validatePassword(String? value) {
     if (value == null || value.trim().isEmpty) {
       return 'Password is required';
     }
 
-    if (value.trim().length < 6) {
-      return 'Password must be at least 6 characters';
+    final password = value.trim();
+
+    if (password.length < 6) {
+      return 'Password must be at least 6 characters long';
+    }
+
+    if (!RegExp(r'[A-Z]').hasMatch(password)) {
+      return 'Password must contain at least one uppercase letter';
+    }
+
+    if (!RegExp(r'[a-z]').hasMatch(password)) {
+      return 'Password must contain at least one lowercase letter';
+    }
+
+    if (!RegExp(r'[!@#\$&*~%^()\-_=+{}[\]|;:"<>,.?/]').hasMatch(password)) {
+      return 'Password must contain at least one special character';
     }
 
     return null;
