cba3653822c4f948c52f2b7de7348fd87078c005
DevNadimm <nadimchowdhury87@gmail.com>
2025-07-01 17:59:26 +0600

    update

--- lib/features/auth/presentation/pages/sign_up_page.dart
+++ lib/features/auth/presentation/pages/sign_up_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:story_lab/core/utils/validators.dart';
 import 'package:story_lab/core/widgets/custom_elevated_button.dart';
+import 'package:story_lab/features/auth/presentation/blocs/auth_bloc.dart';
 import 'package:story_lab/features/auth/presentation/pages/sign_in_page.dart';
 import 'package:story_lab/features/auth/presentation/widgets/auth_footer_text.dart';
 
 class SignUpPage extends StatefulWidget {
   static route() => MaterialPageRoute(builder: (_) => const SignUpPage());
+
   const SignUpPage({super.key});
 
   @override
@@ -21,64 +24,72 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Form(
-          key: _globalKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Sign Up.",
-                style: Theme.of(context).textTheme.displayLarge,
-              ),
-              const SizedBox(height: 24),
-              TextFormField(
-                controller: _fullNameController,
-                keyboardType: TextInputType.name,
-                textInputAction: TextInputAction.next,
-                decoration: const InputDecoration(
-                  hintText: "Full Name",
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailure) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.errorMessage)));
+            print(state.errorMessage);
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Form(
+            key: _globalKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Sign Up.",
+                  style: Theme.of(context).textTheme.displayLarge,
                 ),
-                validator: (value) => Validators.validateRequired(value, fieldName: "Full name"),
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-                decoration: const InputDecoration(
-                  hintText: "Email",
+                const SizedBox(height: 24),
+                TextFormField(
+                  controller: _fullNameController,
+                  keyboardType: TextInputType.name,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    hintText: "Full Name",
+                  ),
+                  validator: (value) => Validators.validateRequired(value, fieldName: "Full name"),
                 ),
-                validator: Validators.validateEmail,
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: _passwordController,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                obscuringCharacter: '*',
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: const InputDecoration(
-                  hintText: "Password",
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    hintText: "Email",
+                  ),
+                  validator: Validators.validateEmail,
                 ),
-                validator: Validators.validatePassword,
-              ),
-              const SizedBox(height: 24),
-              CustomElevatedButton(
-                onPressed: _submitButton,
-                label: "Sign Up",
-              ),
-              const SizedBox(height: 16),
-              AuthFooterText(
-                leadingText: 'Already have an account? ',
-                actionText: 'Sign In',
-                onTap: () {
-                  Navigator.pushReplacement(context, SignInPage.route());
-                },
-              )
-            ],
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  obscuringCharacter: '*',
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(
+                    hintText: "Password",
+                  ),
+                  validator: Validators.validatePassword,
+                ),
+                const SizedBox(height: 24),
+                CustomElevatedButton(
+                  onPressed: _submitButton,
+                  label: "Sign Up",
+                ),
+                const SizedBox(height: 16),
+                AuthFooterText(
+                  leadingText: 'Already have an account? ',
+                  actionText: 'Sign In',
+                  onTap: () {
+                    Navigator.pushReplacement(context, SignInPage.route());
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -86,7 +97,15 @@ class _SignUpPageState extends State<SignUpPage> {
   }
 
   _submitButton() {
-    if (_globalKey.currentState?.validate() ?? false) {}
+    if (_globalKey.currentState?.validate() ?? false) {
+      context.read<AuthBloc>().add(
+            AuthSignUp(
+              fullName: _fullNameController.text.trim(),
+              email: _emailController.text.trim(),
+              password: _passwordController.text.trim(),
+            ),
+          );
+    }
   }
 
   @override
