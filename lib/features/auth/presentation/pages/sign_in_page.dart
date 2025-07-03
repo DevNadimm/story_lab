a5b46f0cb9de25ef2c59b98ccea2c8343470010a
DevNadimm <nadimchowdhury87@gmail.com>
2025-07-03 18:06:03 +0600

    update sign in page with password visibility

--- lib/features/auth/presentation/pages/sign_in_page.dart
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -1,11 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:story_lab/core/themes/app_colors.dart';
 import 'package:story_lab/core/utils/validators.dart';
 import 'package:story_lab/core/widgets/custom_elevated_button.dart';
 import 'package:story_lab/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:story_lab/features/auth/presentation/state_management/cubits/password_visibility_cubit.dart';
 import 'package:story_lab/features/auth/presentation/widgets/auth_footer_text.dart';
 
 class SignInPage extends StatefulWidget {
-  static route() => MaterialPageRoute(builder: (_) => const SignInPage());
+  static route() => CupertinoPageRoute(builder: (_) => const SignInPage());
+
   const SignInPage({super.key});
 
   @override
@@ -19,55 +25,70 @@ class _SignInPageState extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Form(
-          key: _globalKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Sign In.",
-                style: Theme.of(context).textTheme.displayLarge,
-              ),
-              const SizedBox(height: 24),
-              TextFormField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-                decoration: const InputDecoration(
-                  hintText: "Email",
+    return BlocProvider(
+      create: (context) => PasswordVisibilityCubit(),
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Form(
+            key: _globalKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Sign In.",
+                  style: Theme.of(context).textTheme.displayLarge,
                 ),
-                validator: Validators.validateEmail,
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: _passwordController,
-                keyboardType: TextInputType.visiblePassword,
-                decoration: const InputDecoration(
-                  hintText: "Password",
+                const SizedBox(height: 24),
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(
+                    hintText: "Email",
+                    prefixIcon: Icon(HugeIcons.strokeRoundedMail01, color: AppColors.textSecondary, size: 22),
+                  ),
+                  validator: Validators.validateEmail,
                 ),
-                obscureText: true,
-                obscuringCharacter: '*',
-                validator: Validators.validatePassword,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-              ),
-              const SizedBox(height: 24),
-              CustomElevatedButton(
-                onPressed: _submitButton,
-                label: "Sign In",
-              ),
-              const SizedBox(height: 16),
-              AuthFooterText(
-                leadingText: 'Don\'t have an account? ',
-                actionText: 'Sign Up',
-                onTap: () {
-                  Navigator.pushReplacement(context, SignUpPage.route());
-                },
-              )
-            ],
+                const SizedBox(height: 16),
+                BlocBuilder<PasswordVisibilityCubit, bool>(
+                  builder: (context, isVisible) {
+                    return TextFormField(
+                      controller: _passwordController,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        prefixIcon: const Icon(HugeIcons.strokeRoundedSquareLockPassword, color: AppColors.textSecondary, size: 22,),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            context.read<PasswordVisibilityCubit>().toggleVisibility();
+                          },
+                          icon: Icon(isVisible ? Icons.visibility_off : Icons.visibility, color: AppColors.textSecondary, size: 22,),
+                        ),
+                      ),
+                      obscureText: !isVisible, // FIXED
+                      obscuringCharacter: '*',
+                      validator: Validators.validatePassword,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                    );
+                  },
+                ),
+                const SizedBox(height: 24),
+                CustomElevatedButton(
+                  onPressed: _submitButton,
+                  label: "Sign In",
+                ),
+                const SizedBox(height: 16),
+                AuthFooterText(
+                  leadingText: 'Don\'t have an account? ',
+                  actionText: 'Sign Up',
+                  onTap: () {
+                    Navigator.pushReplacement(context, SignUpPage.route());
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
