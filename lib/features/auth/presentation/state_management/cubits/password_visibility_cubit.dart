efcdb864bd2b72a4491a7aa9e759e7b801d0ad1c
DevNadimm <nadimchowdhury87@gmail.com>
2025-07-14 19:47:07 +0600

    update

--- lib/features/auth/presentation/state_management/cubits/password_visibility_cubit.dart
+++ lib/features/auth/presentation/state_management/cubits/password_visibility_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class PasswordVisibilityCubit extends Cubit<bool>{
+class PasswordVisibilityCubit extends Cubit<bool> {
   PasswordVisibilityCubit() : super(false);
 
   void toggleVisibility() => emit(!state);
